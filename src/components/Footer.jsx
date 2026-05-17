edce4ee947c22e3a01cc8e2dc394c80cc8eb8535
milagrosgit7-source <milagrosgit7@gmail.com>
2026-05-17 20:51:28 -0300

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2026 Gestión de Proyectos Educativos</p>
+      <p>
+        Trabajo Práctico N° 3 - React 
+        <br />
+        Analista Programador Universitario
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;

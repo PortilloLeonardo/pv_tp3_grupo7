5909b8a7faf2d578aac96c3d171b9450ea462af1
milagrosgit7-source <milagrosgit7@gmail.com>
2026-05-17 20:50:25 -0300

    Add Nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="nav">
+        <li><a href="/">Inicio</a></li>
+        <li><a href="#proyectos">Proyectos</a></li>
+        <li><a href="#about">Sobre Nosotros</a></li>
+        <li><a href="#contact">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;

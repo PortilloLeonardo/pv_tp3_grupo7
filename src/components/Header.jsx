f61dba5dd34cc642dadf6f13682ee3e63385ef5e
Nicolas <nicolasbravoexequiel@gmail.com>
2026-06-05 12:23:33 -0300

    Header.jsx modificado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-
+import '../css/style.css';
+import logo from "../assets/logo.png"
 const Header = () => {
-  return (
-    <header>
-      <div className="presentacion">
+  {/* Componente de encabezado con logo y título */}
+    return (
+      <header>
+        <div className="presentacion">
         <div className="contenendor-logo">
-          {/* Si tenés un logo en src/assets/logo.png, podés usarlo */}
-          <img src="/logo.png" className="logo" alt="Logo" />
+          <img src={logo} className="logo" alt="Logo" />
         </div>
         <div className="titulo">
           <h1>Gestión de Proyectos Educativos</h1>
         </div>
       </div>
-    </header>
-  );
-};
-
+      </header>
+    )
+}
 export default Header;
